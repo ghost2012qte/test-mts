59ddf630f4674d0f2aa1973dbde0cc89491fbbf8
Alex Caulfield <ghost2012qte@yandex.ru>
2021-07-13 15:31:56 +0300

    a1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,6 @@ export class AppComponent implements OnInit {
     ngOnInit() {
 
         this.route.queryParams.subscribe(params => {
-            
         })
 
     }
--- src/app/pages/channels-page/channels-page.component.ts
+++ src/app/pages/channels-page/channels-page.component.ts
@@ -1,39 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AppService } from 'src/app/app.service';
 import { UrlMask } from 'src/app/classes/url-mask';
 import { UrlService } from 'src/app/url.service';
-import { concatAll, take, toArray } from 'rxjs/operators';
+import { concatAll, repeatWhen, take, takeUntil, toArray, filter } from 'rxjs/operators';
 import { IChannel } from 'src/app/app.types';
+import { Subject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-channels-page',
     templateUrl: './channels-page.component.html',
     styleUrls: ['./channels-page.component.scss']
 })
-export class ChannelsPageComponent implements OnInit {
+export class ChannelsPageComponent implements OnInit, OnDestroy{
 
     mask: UrlMask;
-    channels: IChannel;
+    channels: IChannel[];
 
+    channelsCount = 24;
+    sort: string;
+    filter: string;
 
+    destroy$ = new Subject<void>();
 
     constructor(
         private appService: AppService,
-        private urlService: UrlService) { }
+        private urlService: UrlService,
+        private route: ActivatedRoute) { }
 
     ngOnInit() {
-        this.appService
-            .getChannels()
-            .pipe(
-                // @ts-ignore
-                concatAll(),
-                take(24),
-                toArray()
-            )
-            .subscribe(channels => {
-                // this.channels = channels;
-                console.log(channels);
+        this.route.queryParams
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(params => {
+                console.log(params);
+                this.sort = params?.sort || null;
+                this.filter = params?.filter || null;
+                this.loadChannels();
             })
+    }
+
+    async loadChannels() {
+        let channels = await this.appService.getChannels().toPromise();
+
+        if (this.sort) {
+            channels = channels.sort((a, b) => {
+                if (a.name > b.name) return this.sort == 'asc' ?  1 : -1;
+                if (a.name < b.name) return this.sort == 'asc' ?  -1 : 1;
+                return 0;
+            })
+        }
+
+        if (this.filter) {
+
+        }
+
+        this.channels = channels.slice(0, this.channelsCount);
+        console.log(this.channels);
+    }
+
+    ngOnDestroy() {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
+
+    loadMore() {
+        this.channelsCount += 12;
+        this.loadChannels();
+    }
+
+    trackByChannelName(i: number, channel: IChannel) {
+        return channel.name;
     }
 
 }
